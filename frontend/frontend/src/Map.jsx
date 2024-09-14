6f5fd287bf9d7e3a727238c3c5a6893af30b59e4
Rohith vemula <vemularohith31@gmail.com>
2024-09-14 16:34:39 +0530

    Map.jsx

--- frontend/frontend/src/Map.jsx
+++ frontend/frontend/src/Map.jsx
@@ -1,13 +1,63 @@
+// // import React, { useState, useEffect } from 'react';
+// // import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+// // import 'leaflet/dist/leaflet.css';
+// // import L from 'leaflet';
+
+// // // Set up a custom icon for the vehicle marker
+// // const vehicleIcon = new L.Icon({
+// //   iconUrl: 'https://cdn-icons-png.flaticon.com/512/854/854878.png', // Example vehicle icon
+// //   iconSize: [25, 25], // Adjust size as necessary
+// //   iconAnchor: [12.5, 12.5]
+// // });
+
+// // const center = [17.385044, 78.486671];
+
+// // const Map = () => {
+// //   const [vehiclePosition, setVehiclePosition] = useState(center);
+// //   const [path, setPath] = useState([center]);
+
+// //   useEffect(() => {
+// //     const interval = setInterval(() => {
+// //       fetch('http://localhost:5000/api/vehicle') // Fetch from the backend
+// //         .then((response) => response.json())
+// //         .then((data) => {
+// //           const newPosition = [data.latitude, data.longitude];
+// //           setVehiclePosition(newPosition);
+// //           setPath((prevPath) => [...prevPath, newPosition]);
+// //         });
+// //     }, 5000); // Update every 5 seconds
+
+// //     return () => clearInterval(interval);
+// //   }, []);
+
+// //   return (
+// //     <MapContainer center={vehiclePosition} zoom={15} style={{ height: '400px', width: '100%' }}>
+// //       <TileLayer
+// //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+// //         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// //       />
+// //       <Marker position={vehiclePosition} icon={vehicleIcon} />
+// //       <Polyline 
+// //         positions={path} 
+// //         color="#4B0082"  // Deep wine color
+// //         weight={4}       // Thickness of the polyline
+// //         opacity={0.8}    // Opacity of the polyline
+// //       />
+// //     </MapContainer>
+// //   );
+// // };
+
+// // export default Map;
 // import React, { useState, useEffect } from 'react';
-// import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+// import { MapContainer, TileLayer, Marker, Polyline, Popup } from 'react-leaflet';
 // import 'leaflet/dist/leaflet.css';
 // import L from 'leaflet';
 
-// // Set up a custom icon for the vehicle marker
-// const vehicleIcon = new L.Icon({
-//   iconUrl: 'https://cdn-icons-png.flaticon.com/512/854/854878.png', // Example vehicle icon
-//   iconSize: [25, 25], // Adjust size as necessary
-//   iconAnchor: [12.5, 12.5]
+// // Set up the bike icon using the provided URL
+// const bikeIcon = new L.Icon({
+//   iconUrl: 'https://img.icons8.com/?size=100&id=15126&format=png&color=000000', // New bike icon URL
+//   iconSize: [35, 35], // Adjust size as necessary
+//   iconAnchor: [17.5, 17.5] // Anchor at the center
 // });
 
 // const center = [17.385044, 78.486671];
@@ -15,6 +65,7 @@
 // const Map = () => {
 //   const [vehiclePosition, setVehiclePosition] = useState(center);
 //   const [path, setPath] = useState([center]);
+//   const [locations, setLocations] = useState([]);
 
 //   useEffect(() => {
 //     const interval = setInterval(() => {
@@ -30,24 +81,39 @@
 //     return () => clearInterval(interval);
 //   }, []);
 
+//   useEffect(() => {
+//     // Fetch locations data
+//     fetch('http://localhost:5000/api/locations')
+//       .then((response) => response.json())
+//       .then((data) => setLocations(data));
+//   }, []);
+
 //   return (
 //     <MapContainer center={vehiclePosition} zoom={15} style={{ height: '400px', width: '100%' }}>
 //       <TileLayer
 //         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 //         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 //       />
-//       <Marker position={vehiclePosition} icon={vehicleIcon} />
+//       <Marker position={vehiclePosition} icon={bikeIcon} />
 //       <Polyline 
 //         positions={path} 
-//         color="#4B0082"  // Deep wine color
+//         color="#FF6347"  // Tomato color for better visibility
 //         weight={4}       // Thickness of the polyline
 //         opacity={0.8}    // Opacity of the polyline
 //       />
+//       {locations.map((loc, index) => (
+//         <Marker key={index} position={[loc.latitude, loc.longitude]}>
+//           <Popup>{loc.label}</Popup>
+//         </Marker>
+//       ))}
 //     </MapContainer>
 //   );
 // };
 
 // export default Map;
+
+
+
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Polyline, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -67,9 +133,10 @@ const Map = () => {
   const [path, setPath] = useState([center]);
   const [locations, setLocations] = useState([]);
 
+  // Fetch vehicle position data from Render backend
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch('http://localhost:5000/api/vehicle') // Fetch from the backend
+      fetch('https://vechile-movement-backend.onrender.com/api/vehicle') // Fetch from Render backend
         .then((response) => response.json())
         .then((data) => {
           const newPosition = [data.latitude, data.longitude];
@@ -81,9 +148,9 @@ const Map = () => {
     return () => clearInterval(interval);
   }, []);
 
+  // Fetch locations data from Render backend
   useEffect(() => {
-    // Fetch locations data
-    fetch('http://localhost:5000/api/locations')
+    fetch('https://vechile-movement-backend.onrender.com/api/locations')
       .then((response) => response.json())
       .then((data) => setLocations(data));
   }, []);
